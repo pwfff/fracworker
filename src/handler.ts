9387897a6cfc2398647d195e32000fe76f9267ed
Aaron Lisman <alisman@cloudflare.com>
2020-10-02 09:55:54 -0500

    inline supersampling

--- src/handler.ts
+++ src/handler.ts
@@ -44,56 +44,66 @@ function drawFractal(
 
   let width = png.width * supersample
   let height = png.height * supersample
-  let buffer = new Uint8Array(width * height * 3)
 
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
-      // project our image coordinates to coordinates on the imaginary plane
-      let cx = (4 / zoom) * (x / width - 0.5) + (centerX - centerX / zoom)
-      let cy = (4 / zoom) * (y / height - 0.5) + (centerY - centerY / zoom)
-
-      // next point - if this trends towards infinity, we don't fill the pixel
-      let zx = 0
-      let zy = 0
-
-      // iterate until the point escapes or we hit our limit ('infinity')
-      let i = 0
-      let infinity = 20
-      let zn = 0
-      while (i < iterations && zn < infinity) {
-        let xt = zx * zy
-        zx = zx * zx - zy * zy + cx
-        zy = 2 * xt + cy
-        zn = Math.sqrt(zx * zx + zy * zy)
-        i++
-      }
-
-      if (i >= iterations) {
-        continue
-      }
-
-      // fracIter is how far outside our bounds we escaped, for color smoothing
-      let fracIter = Math.log2(Math.log(zn) / Math.log(infinity))
-
-      // norm is some value between 0 and 1, a ratio of how many of the iterations it took to escape
-      const norm = Math.sqrt((i - fracIter) / iterations)
-
-      // map the number of iterations to some periodic color
-      let r = (Math.sin(norm * 20 * 0.3) * 0.5 + 0.5) * 255
-      let g = (Math.sin(norm * 20 * 0.45) * 0.5 + 0.5) * 255
-      let b = (Math.sin(norm * 20 * 0.65) * 0.5 + 0.5) * 255
-
-      let idx = (width * y  + x) * 3
-      buffer[idx + 0] = Math.floor(r)
-      buffer[idx + 1] = Math.floor(g)
-      buffer[idx + 2] = Math.floor(b)
-    }
-  }
-
-  // subsample our larger image, filling in the png
   for (let x = 0; x < png.width; x++) {
     for (let y = 0; y < png.height; y++) {
-      let { r, g, b } = get_rgb_average(buffer, width, supersample, x, y)
+      let x0 = x * supersample
+      let y0 = y * supersample
+
+      let r = 0
+      let g = 0
+      let b = 0
+
+      // get the extra pixels, average using sum of squares
+      for (let xi = x0; xi < x0 + supersample; xi++) {
+        for (let yi = y0; yi < y0 + supersample; yi++) {
+          // project our image coordinates to coordinates on the imaginary plane
+          let cx = (4 / zoom) * (xi / width - 0.5) + (centerX - centerX / zoom)
+          let cy = (4 / zoom) * (yi / height - 0.5) + (centerY - centerY / zoom)
+
+          // next point - if this trends towards infinity, we don't fill the pixel
+          let zx = 0
+          let zy = 0
+
+          // iterate until the point escapes or we hit our limit ('infinity')
+          let i = 0
+          let infinity = 20
+          let zn = 0
+          while (i < iterations && zn < infinity) {
+            let xt = zx * zy
+            zx = zx * zx - zy * zy + cx
+            zy = 2 * xt + cy
+            zn = Math.sqrt(zx * zx + zy * zy)
+            i++
+          }
+
+          if (i >= iterations) {
+            continue
+          }
+
+          // fracIter is how far outside our bounds we escaped, for color smoothing
+          let fracIter = Math.log2(Math.log(zn) / Math.log(infinity))
+
+          // norm is some value between 0 and 1, a ratio of how many of the iterations it took to escape
+          const norm = Math.sqrt((i - fracIter) / iterations)
+
+          // map the number of iterations to some periodic color
+          let ri = (Math.sin(norm * 20 * 0.3) * 0.5 + 0.5) * 255
+          let gi = (Math.sin(norm * 20 * 0.45) * 0.5 + 0.5) * 255
+          let bi = (Math.sin(norm * 20 * 0.65) * 0.5 + 0.5) * 255
+
+          // add to sum of squares
+          r += ri ** 2
+          g += gi ** 2
+          b += bi ** 2
+        }
+      }
+
+      // average the sum of squares
+      r = Math.sqrt(r / (supersample ** 2))
+      g = Math.sqrt(g / (supersample ** 2))
+      b = Math.sqrt(b / (supersample ** 2))
+
       let idx = (png.width * y + x) << 2
       png.data[idx + 0] = Math.floor(r)
       png.data[idx + 1] = Math.floor(g)
@@ -101,50 +111,4 @@ function drawFractal(
       png.data[idx + 3] = 255
     }
   }
-}
-
-function get_rgb(
-  buffer: Uint8Array,
-  width: number,
-  x: number,
-  y: number,
-): { r: number; g: number; b: number } {
-  let idx = (width * y + x) * 3
-  return {
-    r: buffer[idx + 0],
-    g: buffer[idx + 1],
-    b: buffer[idx + 2],
-  }
-}
-
-function get_rgb_average(
-  buffer: Uint8Array,
-  width: number,
-  ratio: number,
-  x: number,
-  y: number,
-): { r: number; g: number; b: number } {
-  // x0/y0 are our starting coordinate in the supersampled buffer
-  let x0 = x * ratio
-  let y0 = y * ratio
-
-  let r = 0
-  let g = 0
-  let b = 0
-
-  // get the extra pixels, average using sum of squares
-  for (let xi = x0; xi < x0 + ratio; xi++) {
-    for (let yi = y0; yi < y0 + ratio; yi++) {
-      let { r: ri, g: gi, b: bi } = get_rgb(buffer, width, xi, yi)
-      r += ri ** 2
-      g += gi ** 2
-      b += bi ** 2
-    }
-  }
-
-  r = Math.sqrt(r / ratio ** 2)
-  g = Math.sqrt(g / ratio ** 2)
-  b = Math.sqrt(b / ratio ** 2)
-
-  return { r, g, b }
 }
